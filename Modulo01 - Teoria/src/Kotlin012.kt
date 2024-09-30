7443fcc0c9a5e66eb1bdf6acddd2b552e4d07c50
FelipeKauanG <felipekauanprofissional@gmail.com>
2024-09-30 02:26:25 -0300

    v1.3

--- Modulo01 - Teoria/src/Kotlin012.kt
+++ Modulo01 - Teoria/src/Kotlin012.kt
@@ -1,8 +1,8 @@
 fun main(){
     println("Qual é o seu nome ?")
-    val nome = readln()
-    println("E quantos ano vocês tem ?")
-    val idade = readln()
+    val nome:String = readln()
+    println("E quantos anos vocês tem ?")
+    val idade:Int = readln().toInt()
     println("Prazer $nome, você tem $idade anos!")
 
 }
